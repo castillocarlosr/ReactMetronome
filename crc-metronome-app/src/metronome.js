7ec649693391f7a1ea66205f7259de0607f52f10
CARLOS R. CASTILLO <castillocarlosr@gmail.com>
2019-09-08 14:12:17 -0700

    will continue on CSS Tuesday

--- crc-metronome-app/src/metronome.js
+++ crc-metronome-app/src/metronome.js
@@ -5,9 +5,22 @@ class Metronome extends Component
 {
     render()
     {
-        //let bpm = 100;
-        //let playing = false;
-        return <div className = "metronome">Lets hear the beats</div>;
+        let bpm = 100;
+        let playing = false;
+        return
+        (
+            <div className = "metronome">
+                <div className = "bpm-slider">
+                    <div>
+                        {bpm} BPM (Beats Per Minute)
+                    </div>
+                    <input type="range" min="30" max="240" value={bpm} />
+                </div>
+                <button> {playing ? 'Stop' : 'Start'} </button>
+            </div>
+        );
+        //return <div className = "metronome">Lets hear the beats</div>;
+        //<input type="range" min="30" max="240" value={bpm}></input>
     }
 }
 
